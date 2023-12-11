24729957337d1b380393d3b6f3dab7806b1e3d88
Ashutoshmishra7305 <150379929+Ashutoshmishra7305@users.noreply.github.com>
2023-12-11 19:35:39 +0530

    Create styling.css

---
+++ styling.css
@@ -0,0 +1,25 @@
+/* styles.css */
+
+body {
+  font-family: 'Arial', sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f5f5f5;
+}
+
+#personal-info {
+  background-color: #333;
+  color: #fff;
+  padding: 20px;
+  text-align: center;
+}
+
+#projects, #accomplishments {
+  padding: 20px;
+}
+
+.project {
+  margin-bottom: 20px;
+}
+
+/* Add more styles as needed */
